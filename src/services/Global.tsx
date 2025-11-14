7c913aac8cd17dc77f6eea3b62611b3dc7d659df
Wendy Katherine Gomez <wendykatherinnegomezv@gmail.com>
2025-11-14 13:50:54 -0500

    enviroment backend

--- src/services/Global.tsx
+++ src/services/Global.tsx
@@ -1,5 +1,5 @@
 //Rutas de conexión
 export const Global = {
     //url: "http://localhost:3900/api/task"
-    url: "https://backendtask-dr2m.onrender.com/api/task"
+    url: "https://backend-task-zeta-lovat.vercel.app/api/task"
 }
